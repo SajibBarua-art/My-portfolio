985b9a473f222d5bd1af803d3c12f742d12fa9e4
SajibBarua-art <sajibbaruanstu@gmail.com>
2021-05-03 17:50:58 +0600

    intro-animation

--- src/Components/Intro/Intro.js
+++ src/Components/Intro/Intro.js
@@ -1,14 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
+import Particles from 'react-particles-js';
+import './Intro.css';
 
 const Intro = () => {
     return (
-        <div className="dark-bg text-white h-100 d-flex align-items-center justify-content-center">
-            <div className="text-center">
-                <p className="intro-font text-wrap">Hello, I'm Sajib Barua.<br /> I'm desperately wanted to be a full-stack web developer.</p>
-                <HashLink smooth to="#portfolio"><button className="btn btn-outline-light me-2">View my work</button></HashLink>
-                <Link to="/Resume-Sajib-v2.pdf" target="_blank" download="Resume-SajibBarua.pdf"><button className="btn btn-outline-light ms-2">Download Resume</button></Link>
+        <div className="dark-bg">
+            <Particles
+                params={{
+                    "particles": {
+                        "number": {
+                            "value": 50
+                        },
+                        "size": {
+                            "value": 2
+                        }
+                    },
+                    "interactivity": {
+                        "events": {
+                            "onhover": {
+                                "enable": true,
+                                "mode": "repulse"
+                            }
+                        }
+                    }
+                }}
+                width="100%"
+                height="100vh"
+
+            />
+            <div id="text-div">
+                <div className="center-all text-center text-info">
+                    <p className="intro-font text-wrap">Hello, I'm Sajib Barua.<br /> I'm desperately wanted to be a full-stack web developer.</p>
+                    <HashLink smooth to="#portfolio"><button className="btn btn-outline-light me-2">View my work</button></HashLink>
+                    <Link to="/Resume-Sajib-v2.pdf" target="_blank" download="Resume-SajibBarua.pdf"><button className="btn btn-outline-light ms-2">Download Resume</button></Link>
+                </div>
             </div>
         </div>
     );
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,14 +4,13 @@ import './Navbar.css';
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark fw-bold navbar">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark fw-bold navbar-style">
             <div className="ms-5 container-fluid">
-                <h1>.</h1>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <ul className="navbar-nav me-auto mb-lg-0">
                         <li className="nav-item">
                             <HashLink className="hash-link-style" smooth to="#home">HOME</HashLink>
                         </li>
